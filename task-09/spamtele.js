2b09644c33523477a5d5d14eb05724f1a8851641
sahith-2k3 <73482292+sahith-2k3@users.noreply.github.com>
2020-11-28 23:41:04 +0530

    Create spamtele.js

---
+++ task-09/spamtele.js
@@ -0,0 +1,10 @@
+var 1 = 0;
+function spam(){
+  setInterval(function(){
+	document.getElementsByClassName('composer_rich_textarea')[0].innerHTML = 'hi';
+	$('.im_submit').trigger('mousedown');	
+	i++;
+	console.log(i + ' MESSAGES SENT');
+  } , 1000 ) ;
+}
+spam()
